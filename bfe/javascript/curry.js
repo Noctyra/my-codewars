c592366290378db1c992aeace165bc1108e26132
mohammad keshavarz <mm.keshavarz@gmail.com>
2024-12-25 22:32:14 +0330

    curry

---
+++ bfe/javascript/curry.js
@@ -0,0 +1,40 @@
+// Currying is a useful technique used in JavaScript applications.
+
+// Please implement a curry() function, which accepts a function and return a curried one.
+
+// Here is an example
+
+// const join = (a, b, c) => {
+//    return `${a}_${b}_${c}`
+// }
+// const curriedJoin = curry(join)
+// curriedJoin(1, 2, 3) // '1_2_3'
+// curriedJoin(1)(2, 3) // '1_2_3'
+// curriedJoin(1, 2)(3) // '1_2_3'
+// more to read
+
+// https://javascript.info/currying-partials
+
+// https://lodash.com/docs/4.17.15#curry
+
+function curry(func) {
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func(...args);
+    } else {
+      return function (...args2) {
+        return curried(...args.concat(...args2));
+      };
+    }
+  };
+}
+
+const join = (a, b, c) => {
+  return `${a}_${b}_${c}`;
+};
+
+const curriedJoin = curry(join);
+
+console.log(curriedJoin(1, 2, 3, 4));
+console.log(curriedJoin(1)(2, 3)); // '1_2_3'
+console.log(curriedJoin(1, 2)(3)); // '1_2_3'
